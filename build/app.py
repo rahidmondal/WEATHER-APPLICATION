c547984a4afa42cb11ad3e65001348acdfb2a1b8
RAHID MONDAL <71984696+rahidmondal@users.noreply.github.com>
2022-09-30 21:03:47 +0530

    varible image work done

--- build/app.py
+++ build/app.py
@@ -3,12 +3,15 @@ from tkinter import messagebox
 import logic_fetch
 import webbrowser
 
+#Initilazing tk window in the starting 
 root = Tk() 
 
 #resources
 app_icon = PhotoImage(file='Resources/icon.png')
 app_background = PhotoImage(file='Resources/bg.png')
-icon = PhotoImage(file='cdn.weatherapi.com/weather/64x64/day/113.png')
+icon_default = PhotoImage(file='cdn.weatherapi.com/weather/64x64/night/200.png')
+
+
 def search():
     try:
         city = city_entry.get()
@@ -16,23 +19,19 @@ def search():
 
         
         results = (f'''
-        City = {logic_fetch.city_name}
-        Time = {logic_fetch.time}
-        Temperature = {logic_fetch.temp_C}
-        Real Feel = {logic_fetch.feelsLikeTemp_C}
-        Condition = {logic_fetch.condition}
-        Humidity = {logic_fetch.humidity}
-        Clouds = {logic_fetch.cloud}
+        City : {logic_fetch.city_name}
+        Time : {logic_fetch.time}
+        Temperature : {logic_fetch.temp_C}
+        Real Feel : {logic_fetch.feelsLikeTemp_C}
+        Condition : {logic_fetch.condition}
+        Humidity : {logic_fetch.humidity}
+        Clouds : {logic_fetch.cloud}
 
         ''')
+        path = logic_fetch.icon
+        final_path = path[2:]
+        icon_default.config(file=final_path) #updated the default image to result icon 
         final_result.set(results)
-        icon_path = logic_fetch.icon
-        global final_icon_path
-        final_icon_path = icon_path[2:]
-        icon_update(final_icon_path)
-        
-
-
 
 
     except : 
@@ -41,18 +40,10 @@ def search():
 
 def callback(url): #Giving credits to weather Api
    webbrowser.open_new_tab(url)
-    
-def icon_update(path:str):
-    icon_updated = PhotoImage(file=path)
-    root.config(icon=icon_updated)
-    updated_icon_area = Label(win,image=icon_updated)
 
-    win.config(icon_area=updated_icon_area)
-    
 def create_app():
    
-    global final_result,icon_photo,icon_area,win
-    final_icon_path = StringVar()
+    global final_result,city_entry
     final_result = StringVar()
 
 
@@ -75,31 +66,25 @@ def create_app():
 
     City_Text = Label(win,text='Enter City').grid(row=2)
     
-    global city_entry
     city_entry = Entry(win)
     city_entry.insert(0,'New Delhi')
     city_entry.grid(row=2,column=2)
 
-
-    result_text = Label(win,text='Results').grid(row=5,column=0)
-
-    result_area = Label(win,textvariable=final_result,bg='White',fg='#1D1A31')
-    result_area.grid(row=6,column=0)
-
-    icon_text = Label(win,text='Condition').grid(row=5,column=2)
-    icon_area = Label(win,image=icon)
-    icon_area.grid(row=6,column=2)
-
     Search_Button = Button(win,text='Search',command=search)
     Search_Button.grid(row=3,column=2)
 
-    
+    result_text = Label(win,text='Results').grid(row=5,column=0)
+
+    result_area = Label(win,textvariable=final_result,bg='White',fg='#1D1A31',relief=SUNKEN)
+
+    result_area.grid(row=6,column=0)
 
 
 
-    
-
-
+    icon_text = Label(win,text='Condition').grid(row=5,column=2)
+    icon_area = Label(win,image=icon_default)
+    icon_area.grid(row=6,column=2)
+   
 
 #Credits Area
     credit_area = Label(root,text=" | Build By: Rahid Mondal ©2022 || Under Development || v-0.2dv ").pack()
@@ -107,14 +92,6 @@ def create_app():
     weather_api_text.pack()
     weather_api_text.bind("<Button-1>", lambda e:
     callback("https://www.weatherapi.com/"))
-
-
-
-
-
-
-
-
 
     #Final Call
     root.mainloop()
