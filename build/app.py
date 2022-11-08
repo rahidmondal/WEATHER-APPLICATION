1db04d17970c687e9504817c260b699108f378b1
RAHID MONDAL <71984696+rahidmondal@users.noreply.github.com>
2022-11-08 22:25:34 +0530

    Basic layout of the new app.py

--- build/app.py
+++ build/app.py
@@ -4,5 +4,32 @@ Description : python file which  controls the GUI
 Version : v.04dv 
 Status : Under Development
 """
+import PySimpleGUI as gui
+
+#Theme 
+gui.theme('BlueMono')
+
+#Layout 
+layout = [
+    [gui.Text('날씨[Nalssi] - Weather Application',size=(50,2),background_color="#BBB5F1")],
+    [gui.Text("Enter City"),gui.Input(default_text="New Delhi",key="City")],
+    [gui.Button('Search')],
+    [gui.Text("Result"),gui.Text("Condition")],
+    [gui.Text("",key='Result'),gui.Image("")]
+
+]
+
+#Window Declaration 
+window = gui.Window("날씨[Nalssi] - Weather Application",layout,icon="Resources\\bg.png",titlebar_icon="Resources\\bg.png")
 
 
+#Event loop
+while True :
+    event,values = window.read()
+    print(event,values)
+    if event == gui.WIN_CLOSED or event == "Exit" :
+        break
+    if event == 'Button':
+        print("You Pressed the button")
+
+window.close()
